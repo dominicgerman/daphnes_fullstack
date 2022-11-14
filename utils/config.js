501b7d5f4c6d1bc2cbcca8268d4c0ca2adfa528b
dominicgerman <dominicgerman@gmail.com>
2022-11-14 12:04:06 -0600

    added imgs for 2 drinks

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-// const SECRET = process.env.SECRET
+const SECRET = process.env.SECRET
 
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
@@ -12,5 +12,5 @@ const MONGODB_URI =
 module.exports = {
   MONGODB_URI,
   PORT,
-  //   SECRET,
+    SECRET,
 }
